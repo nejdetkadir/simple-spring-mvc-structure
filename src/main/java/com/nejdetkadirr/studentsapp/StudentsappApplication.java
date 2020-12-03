c3129503cbe0b1e566ae8b363827e6fd1fbcec50
nejdetkadir <nejdetkadir.550@gmail.com>
2020-12-03 05:54:02 +0300

    Init spring project

---
+++ src/main/java/com/nejdetkadirr/studentsapp/StudentsappApplication.java
@@ -0,0 +1,13 @@
+package com.nejdetkadirr.studentsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nejdetkadirr/studentsapp/StudentsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nejdetkadirr.studentsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
